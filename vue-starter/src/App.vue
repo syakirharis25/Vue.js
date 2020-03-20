078dda31d91e1f16f44f215320f678317475fdc7
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-20 08:20:28 +0800

    new file:   vue-starter/.browserslistrc         new file:   vue-starter/.eslintrc.js         new file:   vue-starter/.gitignore         new file:   vue-starter/README.md         new file:   vue-starter/babel.config.js         new file:   vue-starter/package-lock.json         new file:   vue-starter/package.json         new file:   vue-starter/public/favicon.ico         new file:   vue-starter/public/index.html         new file:   vue-starter/src/App.vue         new file:   vue-starter/src/PostService.js         new file:   vue-starter/src/assets/logo.png         new file:   vue-starter/src/components/HelloWorld.vue         new file:   vue-starter/src/components/Navbar.vue         new file:   vue-starter/src/components/PostForm.vue         new file:   vue-starter/src/main.js         new file:   vue-starter/src/router.js         new file:   vue-starter/src/views/About.vue         new file:   vue-starter/src/views/Home.vue

---
+++ vue-starter/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
